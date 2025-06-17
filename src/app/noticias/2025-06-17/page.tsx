929a59cdd20ec77346a2e5890f230f30187603b5
Diego Pires <diegocpires@gmail.com>
2025-06-17 14:58:15 -0300

    Fix news

--- src/app/noticias/2025-06-17/page.tsx
+++ src/app/noticias/2025-06-17/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
+import Navbar from "../../../components/Navbar";
+import Footer from "../../../components/Footer";
 
 export default function Servicos() {
   return (
